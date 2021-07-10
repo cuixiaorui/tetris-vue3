76a7537443c7704ac444a6e18cd6653d8a1f917f
cuixiaorui <cui_xiaorui@126.com>
2021-07-10 17:37:10 +0800

    add server

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const Koa = require("koa");
+const app = new Koa();
+const server = require("http").createServer(app.callback());
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+    credentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  // 1. 接受 user 发过来的数据
+  // 2. 广播给其他的 user
+  socket.on("eliminateLine", (num) => {
+    // 玩家消行了
+    // 1. 让其他的玩家加行
+    // 2. 同步自己的 Dival 视图
+    socket.emit("syncAddLine", num);
+    socket.broadcast.emit("addLine", num);
+  });
+  socket.on("moveBoxToDown", (info) => {
+    socket.broadcast.emit("moveBoxToDown", info);
+  });
+  socket.on("moveBoxToLeft", (info) => {
+    socket.broadcast.emit("moveBoxToLeft", info);
+  });
+  socket.on("moveBoxToRight", (info) => {
+    socket.broadcast.emit("moveBoxToRight", info);
+  });
+  socket.on("rotateBox", (info) => {
+    socket.broadcast.emit("rotateBox", info);
+  });
+  socket.on("createBox", (info) => {
+    socket.broadcast.emit("createBox", info);
+  });
+});
+
+server.listen(3001, () => {
+  console.log("listening on *:3001");
+});
