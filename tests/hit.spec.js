1aac4c5f46e9ffcf9ad63c7bbb86928c71d300bf
cuixiaorui <cui_xiaorui@126.com>
2021-07-20 11:53:50 +0800

    add test

--- tests/hit.spec.js
+++ tests/hit.spec.js
@@ -1,43 +1,57 @@
-import { hitBottomBox, hitLeftBox } from "../src/game/hit.js";
 import { Box } from "../src/game/Box";
+import { hitBottomBoundary, hitBottomBox } from "../src/game/hit";
+import { gameRow } from "../src/game/config";
 
-test("bottom", () => {
-  const map = [
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0],
-    [0, -1, 0, 0, 0],
-  ];
+describe("hit", () => {
+  it("should hit when box move to game bottom boundary", () => {
+    const box = new Box();
+    box.y = gameRow;
 
-  const box = new Box({ x: 0, y: 1 });
-
-  box.shape = [
-    [0, 0, 3],
-    [0, 3, 3],
-    [0, 3, 0],
-  ];
-
-  expect(hitBottomBox(box, map)).toBe(true);
-});
-
-describe("left", () => {
-  it("not collision", () => {
-    const map = [
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, 0, 0, 0, 0],
-      [0, -1, 0, 0, 0],
-    ];
-
-    const box = new Box({ x: 1, y: 0 });
-    box.shape = [
-      [2, 0, 0],
-      [2, 2, 0],
-      [0, 2, 0],
-    ];
-
-    expect(hitLeftBox(box, map)).toBe(false);
+    expect(hitBottomBoundary(box)).toBe(true);
   });
+  it("当box没有超出游戏底部边界的时候应该返回 false", () => {
+    const box = new Box();
+    box.y = 1;
+
+    expect(hitBottomBoundary(box)).toBe(false);
+  });
+
+  it("box 在出界的边缘", () => {
+    const box = new Box();
+    box.shape = [
+      [1, 1],
+      [1, 1],
+    ];
+
+    box.y = gameRow - box.shape.length - 1;
+
+    expect(hitBottomBoundary(box)).toBe(false);
+  });
+
+  it("当向下移动碰到其他的 box 的时候，需要停下来", async () => {
+    const box = new Box();
+    box.shape = [
+      [1, 0, 0],
+      [1, 1, 0],
+      [0, 1, 0],
+    ];
+
+    const map = [
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [0, 0, 0, 0],
+      [-1, 0, 0, 0],
+      [-1, -1, 0, 0],
+      [0, -1, 0, 0],
+    ];
+
+    box.x = 1;
+    box.y = 2;
+    expect(hitBottomBox(box, map)).toBe(true);
+    // box.y = 2;
+    // expect(hitBottomBox(box, map)).toBe(true);
+  });
+
+  //   it.todo("当向左移动碰到其他的 box 的时候，需要停下来", () => {});
+  //   it.todo("当向右移动碰到其他的 box 的时候，需要停下来", () => {});
 });
--- tests/matrix.spec.js
+++ tests/matrix.spec.js
@@ -1,110 +1,99 @@
 import {
+  getBottomPoints,
   getLeftPoints,
   getRightPoints,
   rotate,
-  rotate180,
-  rotate270,
 } from "../src/game/matrix";
+
 describe("matrix", () => {
-  describe("获取边界点", () => {
-    test("获取 matrix 左侧的边界点", () => {
-      const matrix = [
-        [0, 0, 3],
-        [0, 3, 3],
-        [0, 3, 0],
-      ];
+  it("获取 box 底部有值的点", () => {
+    const matrix = [
+      [1, 1, 1],
+      [1, 1, 1],
+    ];
 
-      expect(getLeftPoints(matrix)).toEqual([
-        { x: 2, y: 0 },
-        { x: 1, y: 1 },
-        { x: 1, y: 2 },
-      ]);
-    });
-
-    test("获取 matrix 右侧的边界点", () => {
-      const matrix = [
-        [0, 0, 3],
-        [0, 3, 3],
-        [0, 3, 0],
-      ];
-
-      expect(getRightPoints(matrix)).toEqual([
-        { x: 2, y: 0 },
-        { x: 2, y: 1 },
-        { x: 1, y: 2 },
-      ]);
-    });
+    expect(getBottomPoints(matrix)).toEqual([
+      { x: 0, y: 1 },
+      { x: 1, y: 1 },
+      { x: 2, y: 1 },
+    ]);
   });
 
-  describe("Rotate", () => {
-    it("rotate 逆时针90度旋转 ", () => {
-      const matrix = [
-        [0, 1, 1],
-        [1, 1, 0],
-        [0, 0, 0],
-      ];
+  it("获取复杂 box 底部有值的点", () => {
+    const matrix = [
+      [1, 0, 0],
+      [1, 1, 0],
+      [0, 1, 0],
+    ];
 
-      // 90
-      expect(rotate(matrix)).toEqual([
-        [1, 0, 0],
-        [1, 1, 0],
-        [0, 1, 0],
-      ]);
+    expect(getBottomPoints(matrix)).toEqual([
+      { x: 0, y: 1 },
+      { x: 1, y: 2 },
+    ]);
+  });
 
-      // 180
-      expect(rotate(rotate(matrix))).toEqual([
-        [0, 0, 0],
-        [0, 1, 1],
-        [1, 1, 0],
-      ]);
+  it("获取 box 左侧有值的点", () => {
+    const matrix = [
+      [0, 1, 1],
+      [1, 1, 1],
+      [1, 0, 1],
+    ];
 
-      // // 270
-      expect(rotate(rotate(rotate(matrix)))).toEqual([
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 0, 1],
-      ]);
+    expect(getLeftPoints(matrix)).toEqual([
+      { x: 1, y: 0 },
+      { x: 0, y: 1 },
+      { x: 0, y: 2 },
+    ]);
+  });
 
-      // 0
-      expect(rotate(rotate(rotate(rotate(matrix))))).toEqual([
-        [0, 1, 1],
-        [1, 1, 0],
-        [0, 0, 0],
-      ]);
-    });
+  it("获取 box 右侧有值的点", () => {
+    const matrix = [
+      [0, 1, 0],
+      [0, 1, 1],
+      [0, 0, 1],
+    ];
 
-    it("逆时针旋转 180 度", () => {
-      const matrix = [
-        [0, 1, 1],
-        [1, 1, 0],
-        [0, 0, 0],
-      ];
+    expect(getRightPoints(matrix)).toEqual([
+      { x: 1, y: 0 },
+      { x: 2, y: 1 },
+      { x: 2, y: 2 },
+    ]);
+  });
+});
+describe("rotate", () => {
+  it("rotate 逆时针90度旋转 ", () => {
+    const matrix = [
+      [0, 1, 1],
+      [1, 1, 0],
+      [0, 0, 0],
+    ];
 
-      expect(rotate180(matrix)).toEqual([
-        [0, 0, 0],
-        [0, 1, 1],
-        [1, 1, 0],
-      ]);
+    // 90
+    expect(rotate(matrix)).toEqual([
+      [1, 0, 0],
+      [1, 1, 0],
+      [0, 1, 0],
+    ]);
 
-      expect(rotate180(rotate180(matrix))).toEqual([
-        [0, 1, 1],
-        [1, 1, 0],
-        [0, 0, 0],
-      ]);
-    });
+    // 180
+    expect(rotate(rotate(matrix))).toEqual([
+      [0, 0, 0],
+      [0, 1, 1],
+      [1, 1, 0],
+    ]);
 
-    it("逆时针旋转 270 度", () => {
-      const matrix = [
-        [0, 1, 1],
-        [1, 1, 0],
-        [0, 0, 0],
-      ];
+    // // 270
+    expect(rotate(rotate(rotate(matrix)))).toEqual([
+      [0, 1, 0],
+      [0, 1, 1],
+      [0, 0, 1],
+    ]);
 
-      expect(rotate270(matrix)).toEqual([
-        [0, 1, 0],
-        [0, 1, 1],
-        [0, 0, 1],
-      ]);
-    });
+    // 0
+    expect(rotate(rotate(rotate(rotate(matrix))))).toEqual([
+      [0, 1, 1],
+      [1, 1, 0],
+      [0, 0, 0],
+    ]);
   });
 });
